33a6465ccfb88baf3fd1a50e0aec1e3b39e2c694
springercass <casprin2@gmail.com>
2019-07-10 15:26:36 -0500

    user router

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // code away!
 const express = require("express");
+const userRouter = require("./users/userRouter");
 
 const server = express();
 
 server.use(express.json());
+server.use("/api/users", userRouter);
 
 const port = 5000;
 server.listen(port, () => {
