4367e1dfec188bffc74b4ea065aecddc048c995e
springercass <casprin2@gmail.com>
2019-07-10 15:17:56 -0500

    initial commit server running

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code away!
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+const port = 5000;
+server.listen(port, () => {
+  console.log(`API running on ${port}`);
+});
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
+});
--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-const express = 'express';
+const express = "express";
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+server.use(json());
+
+const port = 5000;
+server.listen(port, () => {
+  console.log(`API running on ${port}`);
+});
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {
-
-};
+function logger(req, res, next) {}
 
 module.exports = server;
